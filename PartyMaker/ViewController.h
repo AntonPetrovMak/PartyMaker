00a938ed95206e9eea0177716473d71d6d444754
AntonPetrovMak <a2@2s-macbook-pro-2.local>
2016-02-03 13:53:51 +0200

    Initial Commit

---
+++ PartyMaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PartyMaker
+//
+//  Created by Petrov Anton on 03.02.16.
+//  Copyright © 2016 Softheme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
