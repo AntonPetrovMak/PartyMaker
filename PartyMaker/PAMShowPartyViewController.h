3b66e0e62f245e30d4c68d5a44d1aadb25455229
AntonPetrovMak <iMac309@imac309.local>
2016-02-12 00:55:24 +0200

    Commit 9.1 add ViewController "Show parties"

--- PartyMaker/PAMShowPartyViewController.h
+++ PartyMaker/PAMShowPartyViewController.h
@@ -10,4 +10,15 @@
 
 @interface PAMShowPartyViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *partyTypeImageView;
+@property (weak, nonatomic) IBOutlet UILabel *partyNameLabel;
+@property (weak, nonatomic) IBOutlet UITextView *partyDescriptionTextView;
+
+@property (weak, nonatomic) IBOutlet UILabel *partyDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *partyTimeStartLabel;
+@property (weak, nonatomic) IBOutlet UILabel *partyTimeEndLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *locationButton;
+@property (weak, nonatomic) IBOutlet UIButton *editButton;
+@property (weak, nonatomic) IBOutlet UIButton *deleteButton;
 @end
